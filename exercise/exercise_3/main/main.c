17932dc8c252f796d91f934d0d8d50ae5995d651
WilliamEngman24 <williamengman24@gmail.com>
2025-02-05 08:57:39 +0100

    doing a latch and task delay

--- exercise/exercise_3/main/main.c
+++ exercise/exercise_3/main/main.c
@@ -3,6 +3,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+#include "driver/ledc.h"
 
 #define BUTTON_INC GPIO_NUM_4
 #define BUTTON_DEC GPIO_NUM_5
@@ -26,52 +27,91 @@ void app_main(void)
     button_dec.intr_type = GPIO_INTR_DISABLE;
     gpio_config(&button_dec);
 
-    //latch
+    ledc_timer_config_t led_timer =
+    {
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .duty_resolution = LEDC_TIMER_12_BIT,
+        .timer_num = LEDC_TIMER_0,
+        .freq_hz = 4000,
+        .clk_cfg = LEDC_AUTO_CLK
+    };
+    ledc_timer_config(&led_timer);
+    
+    ledc_channel_config_t led_channel = 
+    {
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .channel = LEDC_CHANNEL_0,
+        .timer_sel = LEDC_TIMER_0,
+        .intr_type = LEDC_INTR_DISABLE,
+        .gpio_num = LED,
+        .duty = 0,
+        .hpoint = 0
+    };
+    ledc_channel_config(&led_channel);
+
+
+    int latch = 0;
+
+    int max_duty = ledc_find_suitable_duty_resolution(LEDC_AUTO_CLK, 4000);
+
+    int duty = 0;
 
     while(1) 
     {
+
         int inc_level = gpio_get_level(BUTTON_INC);
         int dec_level = gpio_get_level(BUTTON_DEC);
 
         //if button == low (inc)
-        if (inc_level == 0)  // && latch
+        if (inc_level == 0 && latch == 0)  // && latch
         {
-            printf("increase");
-                /*
-                //if (inc amount + pwm current >= pwm max)
-                if () 
-                {
-                    //debounce if
-                    //sett pwm to max pwm
-                }
+
+            printf("increased");
+                
+            if (duty + 1000 >= max_duty)
+            {
+                //debounce if
+                //sett pwm to max pwm
+                duty = max_duty;
+            }
                     
-                //else if , med latch
-                else 
-                {
-                    //add more pwm
-                }
-                */
+            //else if , med latch
+            else 
+            {
+                duty += 1000;
+                //add more pwm
+            }
+
+            latch = 1;
+
+            //task delay
+
+        }
+        else if (inc_level == 0 && latch == 1) 
+        {
+
         }
                     
         if (dec_level == 0) 
         {
-            printf("decrease");
-                /*
-                //if button == low (dec)
-                //if (dec amount <= pwm max)
-                if() 
-                {
-                    //turn pwm off/no value
-                }
+            printf("decreased");
+            //if (dec amount <= pwm max)
+            if(duty - 1000 <= 0) 
+            {
+                //turn pwm off/no value
+                duty = 0;
+            }
 
-                //else
-                else 
-                {
-                    //add less pwm
-                }
-                */
+            else 
+            {
+                //add less pwm
+                duty -= 1000;
+            }
         } 
             //pwm for led execute
+
+        ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, duty, 0);
+        vTaskDelay(1000/ portTICK_PERIOD_MS);
 
     }
 }
