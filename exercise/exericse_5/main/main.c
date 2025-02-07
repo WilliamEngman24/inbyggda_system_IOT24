509e5028bbda079ba2fdddf90cff7f503764c6c4
WilliamEngman24 <williamengman24@gmail.com>
2025-02-07 15:50:51 +0100

    on I2C

--- exercise/exericse_5/main/main.c
+++ exercise/exericse_5/main/main.c
@@ -7,6 +7,9 @@
 #include "freertos/FreeRTOS.h"
 #include "esp_log.h"
 
+#include "driver/i2c_master.h"
+#include "driver/i2c.h"
+
 
 #define TX GPIO_NUM_5
 #define RX GPIO_NUM_4
@@ -20,8 +23,53 @@
 
 #define SEND_PERIOD 2000
 
+//
+#define I2C_SCL GPIO_NUM_6
+#define I2C_SDA GPIO_NUM_8
+#define DATA_LENGTH 100
+
 void app_main(void)
 {
+    while(1) 
+    {
+        i2c_master_bus_config_t i2c_mst_config = 
+        {
+            .clk_source = LP_I2C_SCLK_DEFAULT,
+            .i2c_port = -1,
+            .scl_io_num = I2C_SCL,
+            .sda_io_num = I2C_SDA,
+            .glitch_ignore_cnt = 7,
+            .flags.enable_internal_pullup = true,
+        };
+
+        i2c_master_bus_handle_t bus_handle;
+        ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
+
+        i2c_device_config_t dev_cfg = {
+            .dev_addr_length = I2C_ADDR_BIT_LEN_7,
+            .device_address = 0x58,
+            .scl_speed_hz = 100000,
+        };
+
+        i2c_master_dev_handle_t dev_handle;
+        ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle));
+
+        uint8_t data_wr[DATA_LENGTH] = "Order: bla bla bla";
+
+        ESP_ERROR_CHECK(i2c_master_transmit(dev_handle, data_wr, DATA_LENGTH, -1));
+
+        uint8_t data_rd[DATA_LENGTH];
+        i2c_master_receive(dev_handle, data_rd, DATA_LENGTH, -1);
+
+        data_rd[DATA_LENGTH] = 0;
+        printf("%s", data_rd);
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+
+
+    // UART
+    /*
     //config UART to port 1
     const uart_port_t uart_num = UART_NUM_1;
 
@@ -109,4 +157,5 @@ void app_main(void)
             vTaskDelay(pdMS_TO_TICKS(1000));
         }
     }
+    */
 }
